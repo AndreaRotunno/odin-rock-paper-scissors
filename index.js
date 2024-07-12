364da848c430b79f70b0ca6caec29f6de0508d8c
Andrea Rotunno <andrea.rotunno@outlook.it>
2024-07-12 17:24:55 +0200

    add logic to play rounds

--- index.js
+++ index.js
@@ -9,9 +9,31 @@ function getComputerChoice() {
       return "scissors";
   }
 
-  function getHumanChoice(){
+  function getHumanChoice() {
     let input = prompt("Enter rock, paper or scissors");
     return input.toLowerCase();
   }
 }
+let playerScore = 0;
+let computerScore = 0;
 
+function playRound(computerChoice, humanChoice) {
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "rock")
+  ) {
+    alert("You Win");
+    playerScore++;
+  } else if (computerChoice === humanChoice) {
+    alert("Draw!");
+  } else {
+    alert("You lose!");
+    computerScore++;
+  }
+  alert(`Your score: ${playerScore}, computer's score: ${computerScore}`)
+}
+
+for (let i = 0; i < 5; i++) {
+  playRound(getComputerChoice(), getHumanChoice())
+}
